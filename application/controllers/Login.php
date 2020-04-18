a42fdee6db8fdad08c903d2cc443fee03e51608a
rifkialfian <61799644+rifkialfian@users.noreply.github.com>
2020-04-18 21:26:59 +0700

    Add files via upload

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -3,23 +3,37 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Login extends CI_Controller {
 
-	/**
-	 * Index Page for this controller.
-	 *
-	 * Maps to the following URL
-	 * 		http://example.com/index.php/welcome
-	 *	- or -
-	 * 		http://example.com/index.php/welcome/index
-	 *	- or -
-	 * Since this controller is set as the default controller in
-	 * config/routes.php, it's displayed at http://example.com/
-	 *
-	 * So any other public methods not prefixed with an underscore will
-	 * map to /index.php/welcome/<method_name>
-	 * @see https://codeigniter.com/user_guide/general/urls.html
-	 */
-	public function index()
-	{
-		$this->load->view('view_Login');
-	}
+   function __construct(){
+      parent::__construct();
+      $this->load->model('model_IG');
+      $this->load->library('session');
+   }
+ 
+   public function index()
+   {
+      $this->load->view('login'); 
+   }
+ 
+   public function aksi_login(){
+      $data['username'] = $this->input->post('username');
+      $data['password'] = $this->input->post('password');
+      if($this->model_IG->login($data)) {
+   	   $this->session->set_userdata('username', $this->input->post('username'));
+         // $data_profile = $this->db->query("select * from profile where username='". $_SESSION['username']. "'");
+         // $row_akun = $data_profile->result();
+
+         // echo $row_akun;
+         // $this->session->set_userdata('name', 'RifkiAlfianAbdiMalik');
+         // $this->session->set_userdata('website', $row_akun['website']);
+         // $this->session->set_userdata('email', $row_akun['email']);
+         // $this->session->set_userdata('bio', $row_akun['bio']);
+         // $this->session->set_userdata('nohp', $row_akun['nohp']);
+         // $this->session->set_userdata('gender', $row_akun['gender']);
+         // echo $row_akun['nama'];
+      redirect('/feed');
+      } else {
+      redirect('/login');
+      }
+   }
 }
+?>
